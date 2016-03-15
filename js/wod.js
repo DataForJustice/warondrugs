51348635233d150a6080a8eadc5776474473db58
pa-w <paw@paw.mx>
2016-03-14 20:19:25 -0400

    Mobile and desktop versions almost ready...

--- js/wod.js
+++ js/wod.js
@@ -40,11 +40,26 @@ $(document).ready (function () {
 				id: "districts",
 				key: "stdin",
 				enumerator: "geometries"
+			},
+			incidents_grid: {
+				type: d3.csv,
+				url: 'data/incidents.grid.csv',
+				id: "incidents_grid",
+				processor: function(rows) { 
+					var nest = new Nestify (rows, ["grid"], ["inv", "arr"]).data,
+						arr = nest.minmean (function (a) { return parseInt (a.values.arr.value); }),
+						inv = nest.minmean (function (a) { return parseInt (a.values.inv.value); }),
+						scale = {
+							arr: d3.scale.sqrt ().domain (arr).range ([0, 4]), 
+							inv: d3.scale.sqrt ().domain (inv).range ([0, 4])
+						}; 
+
+					return {data: nest, scale: scale}
+				}
 			}
 		},
 		prequantifiers: {
 			categorize: function (args) { 
-				console.log (arguments);
 				return {}
 			},
 			population: function (args) { 
@@ -53,10 +68,16 @@ $(document).ready (function () {
 		},
 		quantifiers: {
 			maps: { 
-				categorize: function (p, a, d) { 
-					if (a == "grid") { 
-						return {"r": 1}
+				incidents: function (p, a, d) { 
+					var d = this.data.incidents_grid;
+					var gid = p.properties.id;
+					if (d.data [gid]) {
+						var val = Math.floor (d.scale [a.type] (d.data [gid][a.type].value));
+						var sc = d3.scale.quantize ().domain (d.scale [a.type].domain ()).range ([1, 2, 3, 4]);
+						return {"r": val, "class": "incidents_" + sc (d.data [gid][a.type].value)}
 					}
+				},
+				categorize: function (p, a, z) { 
 					if (a == "neighborhoods") { 
 						var name = p.properties.name.replace(/\//, '_').replace (/\s/,'_').toLowerCase ();
 						return {"class": "neighborhood neighborhood_" + name + " neighborhood_" + p.properties.gid }
@@ -67,7 +88,6 @@ $(document).ready (function () {
 						scale = d3.scale.quantize ().domain ([0, 100]).range ([1,2,3,4]),
 						cls = "blockgroup population black_" + scale (black / total) + " white_" + scale (white / total) + " opoc_" + scale (opoc / total) 
 							+ " poc_" + scale (poc / total);
-					console.log (black + " " + total);
 
 					return {"class": cls}
 
