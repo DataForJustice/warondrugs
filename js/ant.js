e56d2606dac4042cd7b8db1ff662a9f08e03885c
pa-w <paw@paw.mx>
2016-04-06 19:34:45 -0400

    Updated work. Added narration and incorporated improvements in Ant

--- js/ant.js
+++ js/ant.js
@@ -237,7 +237,7 @@ Ant.prototype = {
 		* Callback
 		*/
 		if (data.callback) {
-			var cb = this.data.callbacks [data.callback]
+			var cb = this.conf.callbacks [data.callback]
 			if (cb) {
 				try { 
 					cb.apply (this, [data.callback_args]);
@@ -393,7 +393,7 @@ Ant.prototype = {
 		this.charts [chart].redraw (quantifier.data, qn);
 		this.charts [chart].on ("click", function (a, id, x, el) { this.parseElement (el); }, this); 
 		this.charts [chart].on ("mouseover", function (a, id, x, el) { this.parseElement (el); }, this); 
-		this.charts [chart].on ("mouseout", function (a, id, x, el) { this.parseElement (el); }, this); 
+		//this.charts [chart].on ("mouseout", function (a, id, x, el) { this.parseElement (el); }, this); 
 	},
 	quantifyMap: function (map, layer, quantifier) {
 		if (this.conf.quantifiers && this.conf.quantifiers ["maps"]) {
@@ -489,7 +489,6 @@ Ant.prototype = {
 					for (var i in trigg) {  
 						$("[data-subscribe_media='" + elm.id + "'][data-subscribe_every='" + trigg [i] + "']").each (parseCb (context));
 					}
-					//console.log (currentSecond);
 					$("[data-subscribe_media='" + elm.id + "'][data-subscribe_time='" + currentSecond + "']").each (parseCb (context));
 					obj.currentSecond = currentSecond;
 
@@ -672,24 +671,34 @@ var asChart = function () {
 		if (quantifier) {
 			// this generates a callback that gives us the chance to edit every attribute in the chart element, and edit it with the users' values (class, degrees, x, y, etc).
 			return function (selector, a, i) {  
-				var qn = quantifier;
+				var qn = quantifier, attrs;
 				if (Array.isArray(a)) { //no objects please, only arrays.
 					var rets = [];
 					// this is for a nested collection. It only supports the first two dimensions. AFAIK. 
-					for (var d in a) { 
-						var ret = qn.fn.apply (qn.context, [a [d], qn.args, qn.data]); //calls the users' callback for every item. 
+					var fns = [];
+					if (!i) { 
+						for (var d in a) { 
+							fns.push (a [d]);
+						}
+					} else {
+						fns.push (a [i]);
+					}
+					for (var x in fns) { 
+						var ret = qn.fn.apply (qn.context, [fns [x], qn.args, qn.data]); //calls the users' callback for every item. 
 						if (innerCallback) { 
 							ret = innerCallback.apply (this, [ret]); // calls charts' "inner" callback with users' input.
 						}
 						rets.push (ret);
 					}
-					var attrs = callback.apply (this, [rets]); // calls charts' normal callback with the collected return values from the inner callback;
+					attrs = callback.apply (this, [rets ]); // calls charts' normal callback with the collected return values from the inner callback;
 				} else if (callback) {
 					var ret = qn.fn.apply (qn.context, [a, qn.args, qn.data]);
-					var attrs = callback.apply (this, [ret]); 
+					attrs = callback.apply (this, [ret]); 
 				} else {
-					var attrs = qn.fn.apply (qn.context [a, qn.args, qn.data]);
+					attrs = qn.fn.apply (qn.context [a, qn.args, qn.data]);
 				}
+				this.setElementAttributes (selector, attrs);
+				/*
 				var data = attrs.data;
 				attrs.data = null;
 				d3.select (selector).attr (attrs);
@@ -702,9 +711,28 @@ var asChart = function () {
 						d3.select (selector).attr ("data-" + d, val);
 					}
 				}
+				*/
 			}
 		}
 		return function (selector, d) { d3.select (selector).attr ("class", ""); };
+	}
+	this.setElementAttributes = function (element, oattrs) {
+		//var attrs =  jQuery.extend ({}, oattrs, true), data;
+		var attrs = oattrs, data;
+		if (attrs.data) {
+			data = attrs.data;	
+			attrs.data = null;
+		}
+		element.attr (attrs);
+		if (data) { 
+			for (var d in data) { 
+				var val = data [d];
+				if (val === Object (val)) { 
+					val = JSON.stringify (val);
+				}
+				element.attr ("data-" + d, val);
+			}
+		}
 	}
 	this.init = function (container, conf) {
 		this.conf = conf;
@@ -805,6 +833,7 @@ asChart.call (ant.charts.bars.prototype);
 asBars.call (ant.charts.bars.prototype);
 var asLines = function () {
 	this.redraw  = function (d, quantifier) { 
+		this.callbacks = {};
 		var data = d.data;
 		d.scale.range ([this.height, 0]); // this comes from the prequantifier and it is used by the quantifier 
 		var lines = data; //TODO verify if this works with a single line..
@@ -815,60 +844,105 @@ var asLines = function () {
 		var pointDistance = this.width / itemsMax;
 		var height = this.height;
 
-		var after = function (container) { 
-			return function (rets) { 
-				var ys = [], rs = [];
+		var after = function (container, origAttrs) { 
+			return function (orets, a) { 
+				var ys = [], rs = [], origYs = [];
 				var attrs = {};
 				var cHeight = height;
+				var rets = jQuery.extend({}, orets, true);
 				for (var i in rets) {
-					rets [i].y = cHeight - rets [i].y;
-					ys.push (rets [i].y);
-					rs.push (rets [i].r);
+					var origY = rets [i].y;
+					rets [i].y = 0;
 					rets [i].x = pointDistance * i;
+					rets [i].width = pointDistance;
+					rets [i].height = cHeight; 
+					var rect = container.insert ("rect", ":first-child")
+						.on ("click", this.createCallback ("click"))
+						.on ("mouseover", this.createCallback ("mouseover"));
+					this.setElementAttributes (rect, {data: rets [i]["data"], width: pointDistance, height: cHeight, x: rets [i].x - (pointDistance / 2), y: 0, "class": rets [i]["class"]});
+					rect.classed ("background", true);
+
+					rets [i].y = origY;
 					rets [i].cx = rets [i].x;
 					rets [i].cy = rets [i].y;
-					container.append ("circle").attr (rets [i]);
-					rets [i].y = null;
-				//	$.extend (attrs, rets);
+					origYs.push (rets [i].y);
+					if (origAttrs && origAttrs ["stepped"] == true) {
+						ys.push ({y: rets [i].y, x: rets [i].x - (pointDistance / 2)});
+						ys.push ({y: rets [i].y, x: rets [i].x});
+						ys.push ({y: rets [i].y, x: rets [i].x + (pointDistance / 2)});
+					} else {
+						ys.push ({y: rets [i].y, x: rets [i].x});
+					}
+					rs.push (rets [i].r);
+
+					var circle = container.insert ("circle")
+						.on ("click", this.createCallback ("click"))
+						.on ("mouseover", this.createCallback ("mouseover"));
+					this.setElementAttributes (circle, {"class": rets [i]["class"], x: rets [i].x, y: rets [i].y});
+					if (rets [i].value) {
+						var text = container.append("text").text (rets [i].value);
+						this.setElementAttributes (text, {"class": rets [i]["class"], x: rets [i].x, y: rets [i].y});
+						text.classed ("value", true);
+					}
+					if (rets [i].label) {
+						var text = container.append ("text").text (rets [i].label);
+						this.setElementAttributes (text, {"class": rets [i]["class"], x: rets [i].x, y: cHeight});
+						text.classed ("label", true);
+					}
+					if (rets [i].note) {
+						var text = container.append ("text").text (rets [i].note);
+						this.setElementAttributes (text, {"class": rets [i]["class"], x: rets [i].x, y: 10});
+						text.classed ("note", true);
+
+					}
 				}
-				var x = function (d, e) { return pointDistance * e; };
-				var y = function (d, e) { return ys [e]; };
+				var x = function (d, e) { return d.x; };
+				var y = function (d, e) { return d.y; };
+				var line = container.insert ("path");
 				var svgLine = d3.svg.line ().x (x).y (y);
-				attrs.d = function (t) { return svgLine (ys) };  
-				
+				line.attr ("d", function (t) { return svgLine (ys) })
+					.on ("click", this.createCallback ("click"));
+				this.setElementAttributes (line, origAttrs);
+
+				//FOREGROUND 	
+				for (var i in rets) { 
+					var attrs = {
+						y: origYs [i] - (pointDistance / 2), 
+						x: (pointDistance * i) - (pointDistance / 2), 
+						width: pointDistance, 
+						height: pointDistance, 
+						data: rets [i].data,
+						"class": rets [i]["class"]
+					};
+					var rect = container.append ("rect")
+						.on ("click", this.createCallback ("click"))
+						.on ("mouseover", this.createCallback ("mouseover"));
+					this.setElementAttributes (rect, {y: attrs.y, x: attrs.x, width: attrs.width, height: attrs.height, data: attrs.data});
+					rect.classed ("square", true);
+
+					var col = container.append ("rect")
+						.on ("click", this.createCallback ("click"))
+						.on ("mouseover", this.createCallback ("mouseover"));
+					attrs.height = cHeight - attrs.y;
+					this.setElementAttributes (col, attrs);
+					col.classed ("column", true);
+				}
 
 				return attrs;
 			}
 		}
-		//var qn = this.quantifierCallback (quantifier, after);
 
-		var bar = this.svg.selectAll ("g")
-			.data (lines);
-
-		bar.enter ().append ("g")
-			.attr ("transform", "translate (0, " + this.margin.top + ")")
-			.on ("click", this.createCallback ("mouseover"))
-			.on ("mouseover", this.createCallback ("mouseover"))
-			.on ("mouseout", this.createCallback ("mouseout"));
-		var quantifierCb = this.quantifierCallback; 
-		bar.append ("path")
-			.each (function (d, e) { 
-				var qn = quantifierCb (quantifier, after (bar)); 
-				qn (this, d.values, e);  
-				var attrs = d.attrs;
-				var data = attrs.data;
-				attrs.data = null;
-				d3.select (this).attr (d.attrs); 
-				if (data) { 
-					for (var d in data) { 
-						var val = data [d];
-						if (val === Object (val)) {
-							val = JSON.stringify (val);
-						}
-						d3.select (this).attr ("data-" + d, val);
-					}
-				}
-			})
+		var quantifierCb = this.quantifierCallback, me = this; 
+		this.svg.selectAll ("g").remove (); //HACK lets see later how to UPDATE them the elements instead of just removing all... 
+		for (var i in lines) { 
+			var line = lines [i];
+			var bar = this.svg.append ("g")
+				.on ("click", this.createCallback ("mouseover"))
+				.on ("mouseover", this.createCallback ("mouseover"));
+			var qn = quantifierCb.apply (this, [quantifier, after (bar, line.attrs)]); 
+			qn.apply  (this, [bar, line.values]);  
+			this.setElementAttributes (bar, line.attrs);
+		}
 	}
 	return this;
 }
@@ -1195,7 +1269,6 @@ Scenify.prototype = {
 		}
 	},
 	enterCallback: function (ev) { 
-		console.log ("enter scene");
 		var elm = ev.target.triggerElement ();
 		this.currentScene = ev.target;
 		this.trigger ("scene_enter", [elm]);
